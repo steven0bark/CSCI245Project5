5b0ed5f3504ebdcc263110d003994929f8d97da4
Steven Barker <steven.barker@my.wheaton.edu>
2020-04-01 10:45:35 -0700

    changes to sieve

--- sieve.c
+++ sieve.c
@@ -2,6 +2,22 @@
 
 #include "bitvector.h"
 
+void
+printVec(struct bit_vector *vec)
+{
+	int isFirst, i;
+	printf("{");
+	isFirst = 1;
+	for (i = 0; i < 10; i++)
+		if (bv_contains(vec, i)) {
+			if (!isFirst)
+				printf(", ");
+			printf("%d", i);
+			isFirst = 0;
+		}
+	printf("} \n");
+}
+
 int
 main(void)
 {
@@ -9,16 +25,24 @@ main(void)
 	struct bit_vector *vec;
 
 	printf("Enter max int-> ");
+	printf("After the enter");
 	scanf("%d", &n);
+	printf("After the scan");
 	
+	printf("right before create");	
 	vec = bv_create(n + 1);
-	for(i = 0; i < vec->size; i++)
+	printf("right after create");
+	for(i = 0; i < vec->size; i++){
+		printf("i: %d", i);
 		bv_insert(vec, i);
+	}
 	int a;
 	for(j = 2; j < vec->size; j++){
 		for(a = j+1; j < vec->size; a++){
-			if(j % a == 0)
+			if(j % a == 0){
+				printf("j: %d/na: %d",j ,a); 
 				bv_remove(vec, a);
+			}
 		}
 
 	} 	
