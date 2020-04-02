99e6b72ce55c3e6bcdc4bd97f650f2fcab65550c
Steven Barker <steven.barker@my.wheaton.edu>
2020-04-01 17:49:00 -0700

    Deleted print statments

--- sieve.c
+++ sieve.c
@@ -25,22 +25,16 @@ main(void)
 	struct bit_vector *vec;
 
 	printf("Enter max int-> ");
-	printf("After the enter");
 	scanf("%d", &n);
-	printf("After the scan");
 	
-	printf("right before create");	
 	vec = bv_create(n + 1);
-	printf("right after create");
 	for(i = 0; i < vec->size; i++){
-		printf("i: %d", i);
 		bv_insert(vec, i);
 	}
 	int a;
 	for(j = 2; j < vec->size; j++){
 		for(a = j+1; j < vec->size; a++){
 			if(j % a == 0){
-				printf("j: %d/na: %d",j ,a); 
 				bv_remove(vec, a);
 			}
 		}
